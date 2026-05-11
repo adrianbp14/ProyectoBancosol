f1b3810dfc0a22db5fd85f065f93f9515a5fa35f
iamemar2212 <163301502+iamemar2212@users.noreply.github.com>
2026-05-11 12:58:40 +0200

    creación de la entidad Cadena
    definición de la entidad Cadena con nombre y código para la gestión de supermercados

---
+++ bancosol/bancosol/src/main/java/es/uma/taw/bancosol/entity/Cadena.java
@@ -0,0 +1,23 @@
+package es.uma.taw.bancosol.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cadena")
+@Getter
+@Setter
+public class Cadena {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cadena")
+    private Integer idCadena;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "codigo", nullable = false, length = 10)
+    private String codigo;
+}
