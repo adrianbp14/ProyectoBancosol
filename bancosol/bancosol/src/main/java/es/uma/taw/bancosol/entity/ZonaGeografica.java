eadabdd52df20f7bf8e670bc7e6d9eb6a994e67c
Amador Sierra González <amadorsierragonzalez@gmail.com>
2026-05-13 09:57:50 +0200

    Modificado Localidad.java y añadido ZonaGeografica.java

---
+++ bancosol/bancosol/src/main/java/es/uma/taw/bancosol/entity/ZonaGeografica.java
@@ -0,0 +1,34 @@
+package es.uma.taw.bancosol.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "zona_geografica")
+public class ZonaGeografica {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_zona")
+    @JsonProperty("id_zona")
+    private Integer idZona;
+
+    @Column(name = "nombre", nullable = false)
+    @JsonProperty("nombre")
+    private String nombre;
+
+    /*
+       Opcional: Si quieres acceder a todas las localidades de una zona
+       desde el objeto ZonaGeografica, puedes añadir esta relación:
+
+    @OneToMany(mappedBy = "zona")
+    @JsonIgnore
+    private List<Localidad> localidades;
+    */
+}
